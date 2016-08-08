750b889b869400bea8e3baad81f3b48cd0cc8bdc
Daniel Weissengruber <daniel.weissengruber@derzer.at>
2016-08-08 15:31:35 +0200

    POOWWW, Awesome new Features: now logging all seen pokemon, not just wild, these even dont seem to exist anymore, but also nearby and catchable pokemons

--- pogom/models.py
+++ pogom/models.py
@@ -353,13 +353,15 @@ def parse_map(map_dict, step_location):
                 send_to_webhook('pokemon', webhook_data)
             
             for p in cell.get('wild_pokemons', []):
-                d_t = datetime.utcfromtimestamp(p['expiration_timestamp_ms'] / 1000.0)
-                printPokemon(p['pokemon_id'], p['latitude'],
+                d_t = datetime.utcfromtimestamp(
+                    (p['last_modified_timestamp_ms'] +
+                     p['time_till_hidden_ms']) / 1000.0)
+                printPokemon(p['pokemon_data']['pokemon_id'], p['latitude'],
                              p['longitude'], d_t)
                 pokemons[p['encounter_id']] = {
                     'encounter_id': b64encode(str(p['encounter_id'])),
                     'spawnpoint_id': p['spawn_point_id'],
-                    'pokemon_id': p['pokemon_id'],
+                    'pokemon_id': p['pokemon_data']['pokemon_id'],
                     'latitude': p['latitude'],
                     'longitude': p['longitude'],
                     'disappear_time': d_t
@@ -368,11 +370,11 @@ def parse_map(map_dict, step_location):
                 webhook_data = {
                     'encounter_id': b64encode(str(p['encounter_id'])),
                     'spawnpoint_id': p['spawn_point_id'],
-                    'pokemon_id': p['pokemon_id'],
+                    'pokemon_id': p['pokemon_data']['pokemon_id'],
                     'latitude': p['latitude'],
                     'longitude': p['longitude'],
                     'disappear_time': calendar.timegm(d_t.timetuple()),
-                    'last_modified_time': p['expiration_timestamp_ms'],
+                    'last_modified_time': p['last_modified_timestamp_ms'],
                     'is_lured': False
                 }
 
