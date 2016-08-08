1907e4e562e33af852ac5403c163a2436112d726
Daniel Weissengruber <daniel.weissengruber@derzer.at>
2016-08-08 14:21:07 +0200

    added area gen scripts

---
+++ Tools/Hex-Beehive-Generator/gen_Bgld.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python location_generator.py -lat 47.801445 -lon 16.293613 -st 5 -lp 7 --accounts bgld_accounts.csv -o ../../Bgld.sh
---
+++ Tools/Hex-Beehive-Generator/gen_Wien.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python location_generator.py -lat 48.210759 -lon 16.372372 -st 6 -lp 9 --accounts wien_accounts.csv -o ../../Wien.sh
