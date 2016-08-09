a2e80c2323c3ab4da4181da48c2366954a13b36a
weissi1994 <daniel.weissengruber@gmail.com>
2016-08-09 07:16:47 +0000

    asdasd

--- pogom/models.py
+++ pogom/models.py
@@ -310,8 +310,6 @@ def parse_map(map_dict, step_location):
                     #log.warn("####################### CATCHABLE_POKEMONS #######################")
                     #log.warn(p)
                     d_t = datetime.utcfromtimestamp(p['expiration_timestamp_ms'] / 1000.0)
-                    printPokemon(p['pokemon_id'], p['latitude'],
-                             p['longitude'], d_t)
                     pokemons[p['encounter_id']] = {
                             'encounter_id': b64encode(str(p['encounter_id'])),
                             'spawnpoint_id': p['spawn_point_id'],
@@ -327,8 +325,6 @@ def parse_map(map_dict, step_location):
                     d_t = datetime.utcfromtimestamp(
                             (p['last_modified_timestamp_ms'] +
                             p['time_till_hidden_ms']) / 1000.0)
-                    printPokemon(p['pokemon_data']['pokemon_id'], p['latitude'],
-                            p['longitude'], d_t)
                     pokemons[p['encounter_id']] = {
                             'encounter_id': b64encode(str(p['encounter_id'])),
                             'spawnpoint_id': p['spawn_point_id'],
@@ -339,6 +335,8 @@ def parse_map(map_dict, step_location):
                     }
                     
             for p in pokemons.values():
+                printPokemon(p['pokemon_id'], p['latitude'],
+                             p['longitude'], p['disappear_time'])
                 webhook_data = {
                     'encounter_id': p['encounter_id'],
                     'spawnpoint_id': p['spawnpoint_id'],
